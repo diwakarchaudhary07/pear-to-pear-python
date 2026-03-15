05332593e3cf71fded20044fbfa8cdf2aff3c9ea
diwakar chaudhary <boyroyal4853@gmail.com>
2026-03-15 23:51:07 +0530

    add

--- star _pettren.py
+++ star _pettren.py
@@ -47,3 +47,62 @@ for i in range(1,x):
     for j in range(1,i+1):
         print(j,end="")
     print()    
+
+
+
+
+
+
+
+# <------------------------------------------------------------------------------------------
+
+
+# x =int(input("enter you no:"))
+
+# for i in range(1,x+1,1):
+#     print("*" *i)
+
+x =int(input("enter you no:"))
+for i in range(x,0,-1):
+    print("*"*i)
+
+
+# x =int(input("enter you no:")) 
+# y =int(input("enter you no:"))
+ 
+# for i in range(0,x+1,2):
+#    print("*" *i)
+# for j in range(0,x+2,3):
+#    print("*" *j)
+
+
+# for i in range(1, 8 ):
+#     for j in range(1, 8,2):
+#         if j <= i:
+#             print("*", end="")
+#         else:
+#             print(" ", end="")
+#     print() 
+
+
+ 
+
+# rows = int(input("enter no:"))
+# for i in range(1, rows+1):
+#     print(" " * (rows-i) + "* " * i)
+
+
+
+# rows = int(input("enter your no:"))
+# for i in range(1, rows+1):
+#     print(" " * (rows-i) + "* " * i)
+# for i in range(rows-1, 0, -1):
+#     print(" " * (rows-i) + "* " * i)
+
+x =int(input("enter you no:"))
+
+
+for i in range(1,x):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()   
