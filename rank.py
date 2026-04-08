6a4ad691e76cd9ba9cb961c168d23ef0d95649b4
diwakar chaudhary <boyroyal4853@gmail.com>
2026-04-08 21:58:12 +0530

    add

--- rank.py
+++ rank.py
@@ -4,7 +4,6 @@ b = int(input("Number 2: "))
 c = int(input("Number 3: "))
 d = int(input("Number 4: "))
 
-# Rank nikalne ke liye if-else conditions
 # a ke liye rank
 if (a >= b and a >= c and a >= d):
     rank_a = 1
